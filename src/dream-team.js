54a2db9194e7a7bf5df558b25db51c2cda1707ce
mi8mto <95370082+mi8mto@users.noreply.github.com>
2023-04-11 19:16:21 +0300

    fest: add /dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function createDreamTeam(members) {
+    if(!Array.isArray(members)) return false
+    let Mia;
+      Mia = members.map(item => {
+       if(typeof item == 'string') {
+        item = item.trim();
+         
+       return item[0].toUpperCase()
+    }
+  })
+  return Mia.sort().join('');
 }
 
 module.exports = {
