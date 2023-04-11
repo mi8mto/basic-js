3f2266b0fa55f6f14d83b7434168935b35c2b2c7
mi8mto <95370082+mi8mto@users.noreply.github.com>
2023-04-11 19:03:39 +0300

    feat: add /carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(sampleActivity) {
+  const isTrue = Boolean(sampleActivity);
+  const isString = typeof(sampleActivity) == 'string';
+  const isBig = sampleActivity > 0 && sampleActivity <= 15;
+  
+  if(!isTrue || !isString || !isBig) return false
+  
+     const k = 0.693/HALF_LIFE_PERIOD;
+     const t = Math.log(MODERN_ACTIVITY/sampleActivity)/k;
+  
+      return Math.ceil(t);
 }
 
 module.exports = {
