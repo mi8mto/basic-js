6324bb15b86b355ec58d10190158268fb6e86459
mi8mto <95370082+mi8mto@users.noreply.github.com>
2023-04-17 15:21:25 +0300

    feat: fix now

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -22,146 +22,85 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 
 class VigenereCipheringMachine {
-  constructor(isTrue = true) {
-    this.bool = isTrue;
-    this.en = [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z",
-    ];
+  constructor(isTrue = true) {  
+  this.bool = isTrue;  
+  this.en = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   }
-  maketotalKey(message, key) {
-    let totalKey = "";
-    if (key.length < message.length) {
-      let count = message.split("").reduce((count, item) => {
-        if (this.en.includes(item.toUpperCase())) {
-          return count + 1;
+  makeFullKey(message, key) {
+    let fullKey = '';
+    if(key.length < message.length) {
+      let counter = message.split('').reduce((count,item) => {
+        if(this.en.includes(item.toUpperCase())) {
+          return count + 1
         }
         return count;
-      }, 0);
-      let fullLengthX = Math.floor(count / key.length);
-      let lastPart = count - key.length * fullLengthX;
-      for (let i = 1; i <= fullLengthX; i++) {
-        totalKey = totalKey + key;
+      },0);
+      let fullLengthX = Math.floor(counter/key.length);
+      let lastPart = counter - key.length*fullLengthX;
+      for(let i = 1; i <= fullLengthX; i++) {
+        fullKey = fullKey + key;
       }
-      for (let j = 0; j < lastPart; j++) {
-        totalKey = totalKey + key[j];
+      for(let j = 0; j < lastPart; j++) {
+        fullKey = fullKey + key[j]
       }
     } else {
-      totalKey = key;
+      fullKey = key;
     }
-    return totalKey;
+    return fullKey;
   }
-  makeFinishArray(message, totalKey, decrypt = 0) {
-    let arrayMessage = message.toUpperCase().split("");
-    let arraytotalKey = totalKey.toUpperCase().split("");
-    console.log(arraytotalKey, "array0");
-    if (decrypt === "decrypt") {
-      return arrayMessage.map((item, i) => {
-        if (this.en.includes(item)) {
-          let cryptoLtr =
-            this.en[
-              Math.abs(
-                this.en.lastIndexOf(arrayMessage[i]) -
-                  this.en.findIndex((ltr) => ltr == arraytotalKey[0])
-              )
-            ];
-          arraytotalKey.splice(0, 1);
+  makeFinishArray(message, fullKey, decrypt = 0) {
+    let arrayMessage = message.toUpperCase().split('');
+    let arrayFullKey = fullKey.toUpperCase().split('');
+     
+    if(decrypt === 'decrypt') {
+      return (arrayMessage.map((item,i) => {
+        if((this.en).includes(item)) {              
+          let cryptoLtr = this.en[Math.abs((this.en).lastIndexOf(arrayMessage[i]) - (this.en).findIndex(ltr => ltr == arrayFullKey[0]))]
+          arrayFullKey.splice(0,1);
           return cryptoLtr;
         } else {
           return item;
         }
-      });
+      }))
     } else {
-      return arrayMessage.map((item, i) => {
-        if (this.en.includes(item)) {
-          let cryptoLtr =
-            this.en[
-              this.en.findIndex((ltr) => ltr == arrayMessage[i]) +
-                this.en.findIndex((ltr) => ltr == arraytotalKey[0])
-            ];
+      return (arrayMessage.map((item,i) => {
+        if((this.en).includes(item)) {
+          
+          let cryptoLtr = this.en[(this.en).findIndex(ltr => ltr == arrayMessage[i]) + (this.en).findIndex(ltr => ltr == arrayFullKey[0])]
+          
+         
           return cryptoLtr;
         } else {
           return item;
         }
-      });
-    }
+      }))
+    };
   }
   encrypt(message, key) {
-    if (!message || !key) throw new Error("Incorrect arg!");
-    let totalKey = this.maketotalKey(message, key);
-    let finishArray = this.makeFinishArray(message, totalKey);
-    if (this.bool) {
-      return finishArray.join("");
+    if(!message || !key) throw new Error("Incorrect arguments!")    
+   
+    let fullKey = this.makeFullKey(message,key)
+   
+    let finishArray = this.makeFinishArray(message,fullKey);
+    if(this.bool) {
+      return finishArray.join('');
     } else {
-      return finishArray.reverse().join("");
+      return finishArray.reverse().join('')
     }
   }
   decrypt(encryptedMessage, key) {
-    if (!encryptedMessage || !key) throw new Error("Incorrect arg!");
-    let totalKey = this.maketotalKey(encryptedMessage, key);
-    let finishArray = this.makeFinishArray(
-      encryptedMessage,
-      totalKey,
-      "decrypt"
-    );
-    if (this.bool) {
-      return finishArray.join("");
+    if(!encryptedMessage || !key) throw new Error("Incorrect arguments!")
+
+    let fullKey = this.makeFullKey(encryptedMessage,key)
+  
+    let finishArray = this.makeFinishArray(encryptedMessage,fullKey,'decrypt');
+    if(this.bool) {
+      return finishArray.join('')
     } else {
-      return finishArray.reverse().join("");
+      return finishArray.reverse().join('')
     }
   }
 }
-
 
 module.exports = {
   VigenereCipheringMachine,
